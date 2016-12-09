7fe6d64faff56c4ae3f04326172c2710d4448faa
Tino Goetz <goetz@traum-ferienwohungen.de>
2016-12-09 12:40:02 +0100

    implement book detail view

---
+++ src/app/book-detail/book-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import { BookDataService } from '../shared';
+import 'rxjs/add/operator/switchMap';  // handle more than one observables
+
+@Component({
+  selector: 'bm-book-detail',
+  templateUrl: './book-detail.component.html',
+  styleUrls: ['./book-detail.component.css']
+})
+export class BookDetailComponent implements OnInit {
+
+  private book;
+
+  constructor(
+      private route: ActivatedRoute,
+      private bookService: BookDataService)
+  {}
+
+  ngOnInit() {
+    this.route.params
+        .switchMap(params => this.bookService.getBookByIsbn(params['isbn']))
+          .subscribe(
+            book => this.book = book
+        );
+  }
+}
--- src/app/shared/book-data.service.ts
+++ src/app/shared/book-data.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@angular/core';
 
 // data model for books
 import {Book} from './book'
-import {Observable} from "rxjs";
-import {Http} from'@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class BookDataService {
 
   private readonly books;
+  private basUrl = 'http://localhost:4730/books';
 
   constructor(
       private http:Http
@@ -18,6 +19,14 @@ export class BookDataService {
   getBooks():  Observable<Book[]> {
     return this.http
         .get('http://localhost:4730/books')
+        .map(response => response.json());
+  }
+
+  getBookByIsbn(isbn: string):  Observable<Book[]> {
+    const apiUrl = `${this.basUrl}/${isbn}`;
+
+    return this.http
+        .get(apiUrl)
         .map(response => response.json());
   }
 }
