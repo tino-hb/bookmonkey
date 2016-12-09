36ff2c2c40b1590fe346551b0e80dd41452101af
Tino Goetz <goetz@traum-ferienwohungen.de>
2016-12-09 13:02:04 +0100

    add mapping to bookk detail service call

--- src/app/book-detail/book-detail.component.ts
+++ src/app/book-detail/book-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import { BookDataService, Book } from '../shared';
 import 'rxjs/add/operator/switchMap';  // handle more than one observables
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'bm-book-detail',
@@ -10,7 +12,7 @@ import 'rxjs/add/operator/switchMap';  // handle more than one observables
 })
 export class BookDetailComponent implements OnInit {
 
-  private book:Book;
+  private book;
 
   constructor(
       private route: ActivatedRoute,
@@ -19,7 +21,9 @@ export class BookDetailComponent implements OnInit {
 
   ngOnInit() {
     this.route.params
-        .switchMap(params => this.bookService.getBookByIsbn(params['isbn']))
+        .map(params => params['isbn'])
+        .distinctUntilChanged()
+        .switchMap(isbn => this.bookService.getBookByIsbn(isbn))
           .subscribe(
             book => this.book = book
         );
